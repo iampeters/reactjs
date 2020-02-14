6c889038680079e6dda53723f18206655b63f6eb
Peters Chikezie <peters.chikezie@bankly.ng>
2020-02-14 17:18:30 +0100

    ADDED dotenv

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
 
+require('dotenv').config();
+
 const store = configureStore();
 
 ReactDOM.render(
